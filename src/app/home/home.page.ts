898810be3a831535c644c16d19ee71da8d012cf7
Manju-Paryani <2018.manju.paryani@ves.ac.in>
2022-11-18 12:56:01 +0530

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+// import { HTTP } from '@ionic-native/http/ngx';
+// import { HTTP } from '@awesome-cordova-plugins/http/ngx';
+import {  from, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +13,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  signin_url = 'https://api.cherryko.tk/users/sign_in' 
+  header =  {
+    'Content-Type' : "application/json"
+  };
+  
+  constructor(public commonService: CommonModule,
+    public httpClient: HttpClient) {}
+
+  signin(){
+    let cred= { "user": { "email": "alok@test.com", "password": "mypassword" } }
+    this.httpClient.post(this.signin_url, cred)
+    .subscribe((response: any) => {
+      console.log('signin success',response)
+    })
+    // .catch((response: any) => {  
+    //   console.log('signin err', response)
+    // });
+
+  }
 
 }
