2fac6d9be48a9a1751b36b4b2e57ca59bc1d0ede
Manju-Paryani <2018.manju.paryani@ves.ac.in>
2022-11-18 15:17:16 +0530

    ch

--- src/app/credential-page/credential-page.page.ts
+++ src/app/credential-page/credential-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { type } from 'os';
 
 @Component({
   selector: 'app-credential-page',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credential-page.page.scss'],
 })
 export class CredentialPagePage implements OnInit {
+  username: string = '';
+  password: any;
+  fullname: string = '';
+  email: string = '';
+  otp!: number;
+  mobileNo: number | undefined;
+  set_password: any;
+  confirm_password: any;
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
