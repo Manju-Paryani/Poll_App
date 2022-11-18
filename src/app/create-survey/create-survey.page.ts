6a99dd63afcbf15a977e83f70f1e38518a7ad312
Manju-Paryani <2018.manju.paryani@ves.ac.in>
2022-11-18 16:00:04 +0530

    ch

--- src/app/create-survey/create-survey.page.ts
+++ src/app/create-survey/create-survey.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-survey',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-survey.page.scss'],
 })
 export class CreateSurveyPage implements OnInit {
-
-  constructor() { }
+  question: string = '';
+  option1:any; 
+  option2: any;
+  option3: any;
+  option4: any;
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  options(){
+    let options = {
+      question_1: this.option1,
+      question_2: this.option2,
+      question_3: this.option3,
+      question_4: this.option4,
+    }
+  }
+
+  dashboard(){
+    this.navCtrl.navigateForward('/credential-page') // change later
+  }
+
+  survey(){
+    this.navCtrl.navigateForward('/create-survey') 
   }
 
 }
