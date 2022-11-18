0d1e9f75d5fdb496c64bd88b06dfe84fb23a05b7
SunilKumar <ssunilkumar220590@gmail.com>
2022-11-18 17:04:57 +0530

    updated

---
+++ src/app/answer-survey/answer-survey.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-survey',
+  templateUrl: './answer-survey.page.html',
+  styleUrls: ['./answer-survey.page.scss'],
+})
+export class AnswerSurveyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/credential-page/credential-page.page.ts
+++ src/app/credential-page/credential-page.page.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credential-page.page.scss'],
 })
 export class CredentialPagePage implements OnInit {
-
+  passwordIcon1: string = 'eye-off';
+  show: boolean = false;
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  hideShowPassword() {
+    this.show = !this.show;
+    this.passwordIcon1 = this.passwordIcon1 === 'eye-off' ? 'eye-on' : 'eye-off';
+  }
+  ey1: any = false;
+  password: any = "";
+  btnCheck = false
+  checkEvent() {
+    if (this.password && this.password.length > 0) {
+      this.ey1 = true;
+    }
+    else {
+      this.ey1 = false;
+    }
+    if (this.password && this.password.length > 0) {
+      this.btnCheck = true
+      return true;
+    }
+    else {
+      this.btnCheck = false
+      return false;
+    }
   }
 
 }
