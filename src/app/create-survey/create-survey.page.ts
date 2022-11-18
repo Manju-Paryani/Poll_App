63a807ccac979c8ce9d4d484ff945e05db2af54b
Manju-Paryani <2018.manju.paryani@ves.ac.in>
2022-11-18 16:50:50 +0530

    ch

--- src/app/create-survey/create-survey.page.ts
+++ src/app/create-survey/create-survey.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-survey',
@@ -7,31 +7,71 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./create-survey.page.scss'],
 })
 export class CreateSurveyPage implements OnInit {
+  category: string = '';
   question: string = '';
   option1:any; 
   option2: any;
   option3: any;
   option4: any;
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController,
+    private alertController: AlertController) { }
 
   ngOnInit() {
   }
-
+  poll_data:any
   options(){
-    let options = {
-      question_1: this.option1,
-      question_2: this.option2,
-      question_3: this.option3,
-      question_4: this.option4,
+    this.poll_data = {
+    topic : '',
+    options :{
+      option_1: this.option1,
+      option_2: this.option2,
+      option_3: this.option3,
+      option_4: this.option4,
     }
   }
+} 
 
   dashboard(){
-    this.navCtrl.navigateForward('/credential-page') // change later
+    this.navCtrl.navigateForward('/my-dashboard') // change later
   }
 
-  survey(){
-    this.navCtrl.navigateForward('/create-survey') 
-  }
+  // survey(){
+    
+    
 
+  //   this.navCtrl.navigateForward('/create-survey') 
+  // }
+
+  async survey(){
+
+    const alert = await this.alertController.create({
+      header: 'Successfully',
+      message : `You have submitted the Poll.`,
+      buttons: [
+        {
+          text : `Go to Dashboard`,
+          cssClass: 'secondary',
+          handler: () => {
+            this.navCtrl.navigateForward('/my-dashboard')
+            alert.dismiss();
+          }
+        }, 
+        {
+          text : `New Poll`,
+          cssClass: 'primary',
+          handler: () => {
+            this.option1 = this.option2 = this.option3 = this.option4 = this.question = this.category = ''
+            // this.navCtrl.navigateForward('/create-survey')
+
+            alert.dismiss();
+          }
+        }
+      ],
+      cssClass: 'cherryAlert cherryBackConfirmation',
+      mode: 'ios'
+    });
+    await alert.present();
+    alert.onDidDismiss().then(() => {
+    })
+ }
 }
